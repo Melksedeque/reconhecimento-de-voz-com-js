e25f7ea3d249171e950e4baf715386fa293b29be
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-25 17:41:28 -0300

    Ajuste na função onSpeak()

--- assets/js/voz.js
+++ assets/js/voz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.querySelector('[data-mensagem="chute"]')
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new SpeechRecognition()
 recognition.lang = 'pt-Br'
@@ -6,5 +8,15 @@ recognition.start()
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    console.log(e.results[0][0].transcript)
+    chute = e.results[0][0].transcript
+    console.log(chute)
+    exibeChute(chute)
+}
+
+function exibeChute(numero) {
+    elementoChute.innerHTML = `
+        <h3>Você disse:</h3>
+        <div class="box valor">${$numero}</div>
+        <div class="alert">O número secreto é maior <i class="fa fa-light fa-arrow-up"></i></div>
+    `
 }
