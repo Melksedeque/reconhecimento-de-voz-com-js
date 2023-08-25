7caf62b1d15a54ecab770337fa2f8a76931015fa
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-25 17:19:38 -0300

    Importando todos os JS em um lugar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,2 @@
-const min = 1
-const max = 100
-
-const numeroSecreto = gerarNumero()
-const menorValor = document.querySelector('[data-valor="menor"]')
-const maiorValor = document.querySelector('[data-valor="maior"]')
-
-menorValor.innerHTML = min
-maiorValor.innerHTML = max
-
-console.log(menorValor)
-
-function gerarNumero() {
-    return parseInt(Math.random() * (max + 1))
-}
-
-console.log(numeroSecreto)
+import './numero'
+import './voz'
