843cfcfa95f5a4298561f2299bf105056c890402
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-27 12:42:38 -0300

    Adicionado o return nos ifs para interromper a execução em caso de true

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -3,18 +3,22 @@ function validaChute(chute) {
 
     if(Number.isNaN(numero)) {
         elementoMensagem.innerText = "Número inválido"
+        return
     }
 
     if(numero > max || numero < min) {
         elementoMensagem.innerText = `Número não permitido: O número secreto está entre ${min} e ${max}`
+        return
     }
 
     if(numero < numeroSecreto) {
         elementoMensagem.innerHTML = 'O número secreto é maior <i class="fa fa-light fa-arrow-up"></i>'
+        return
     }
 
     if(numero > numeroSecreto) {
         elementoMensagem.innerHTML = 'O número secreto é menor <i class="fa fa-light fa-arrow-down"></i>'
+        return
     }
 
     if(numero === numeroSecreto) {
