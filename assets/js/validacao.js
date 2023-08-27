6279d13cdfedd019e6ee9fe3e7b5a5606e186dae
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-27 12:33:42 -0300

    Função de validação do numero falado

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -2,10 +2,27 @@ function validaChute(chute) {
     const numero = +chute
 
     if(Number.isNaN(numero)) {
-        console.log("Número inválido")
+        elementoMensagem.innerText = "Número inválido"
     }
 
     if(numero > max || numero < min) {
-        console.log("Número não permitido")
+        elementoMensagem.innerText = `Número não permitido: O número secreto está entre ${min} e ${max}`
+    }
+
+    if(numero < numeroSecreto) {
+        elementoMensagem.innerHTML = 'O número secreto é maior <i class="fa fa-light fa-arrow-up"></i>'
+    }
+
+    if(numero > numeroSecreto) {
+        elementoMensagem.innerHTML = 'O número secreto é menor <i class="fa fa-light fa-arrow-down"></i>'
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <main class="page-main">
+                <h1>Você acertou!</h1>
+                <h2>O número secreto era: ${numeroSecreto}</h2>
+            </main>
+        `
     }
 }
